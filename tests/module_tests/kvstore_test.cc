750f4a6abbd19ae90839eeb899fc9c40d5e011d7
Volker Mische <volker.mische@gmail.com>
2016-10-30 15:31:42 -0700

    Make KVstore unit test work
    The KVStore test with LevelDB as backend can be run via:
    
        ./ep-engine_ep_unit_tests \
        --gtest_filter=CouchstoreAndForestDB/CouchAndForestTest.BasicTest/leveldb
    
    Change-Id: Id03ce61c368318b0d8893b7b2801d421b03beb6a

--- tests/module_tests/kvstore_test.cc
+++ tests/module_tests/kvstore_test.cc
@@ -1650,14 +1650,14 @@ TEST_F(CouchKVStoreMetaData, assignment) {
 // Test cases which run on both Couchstore and ForestDB
 INSTANTIATE_TEST_CASE_P(CouchstoreAndForestDB,
                         CouchAndForestTest,
-                        ::testing::Values("couchdb", "forestdb"),
+                        ::testing::Values("couchdb", "forestdb", "leveldb"),
                         [] (const ::testing::TestParamInfo<std::string>& info) {
                             return info.param;
                         });
 #else
 INSTANTIATE_TEST_CASE_P(CouchstoreAndForestDB,
                         CouchAndForestTest,
-                        ::testing::Values("couchdb"),
+                        ::testing::Values("couchdb", "leveldb"),
                         [] (const ::testing::TestParamInfo<std::string>& info) {
                             return info.param;
                         });
